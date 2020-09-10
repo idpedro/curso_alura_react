d1a23be608a5341718c984196678daa8bda7ec7d
Pedro Ferreira <pedroferreira@ferreirasolucoes.site>
2020-09-09 23:18:40 -0300

    Primeiros passo com o react

---
+++ react_entendendo_a_bibli/ceep/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import Welcome from './pages/welcome'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Welcome/>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
---
+++ react_entendendo_a_bibli/ceep/src/pages/welcome/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+const Welcome = ()=>{
+    return (
+        <h1>Bem vindo mundo</h1>
+    )
+}
+
+export default Welcome;
